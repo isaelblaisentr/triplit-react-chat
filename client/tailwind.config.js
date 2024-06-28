8862bff500caf1af29da96ec86edc92c462c1390
isabla <blais.isael.entr@gmail.com>
2024-06-28 19:33:55 -0400

    Wrap tailwind config withMT

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+import withMT from '@material-tailwind/react/utils/withMT';
+
+const twConfig = withMT({
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});
+export default twConfig;
