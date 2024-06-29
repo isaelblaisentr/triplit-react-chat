629ba92030e523448d4f53d9fc7fb90c371904e7
isabla <blais.isael.entr@gmail.com>
2024-06-29 17:43:32 -0400

    Edit schema for Conversations & UserConversation many to many relation.

--- client/triplit/schema.ts
+++ client/triplit/schema.ts
@@ -1,4 +1,4 @@
-import { ClientSchema, Schema as S } from "@triplit/client";
+import { ClientSchema, Schema as S } from '@triplit/client';
 
 /**
  * Define your schema here. After:
@@ -8,35 +8,41 @@ import { ClientSchema, Schema as S } from "@triplit/client";
  * For more information about schemas, see the docs: https://www.triplit.dev/docs/schemas
  */
 export const schema = {
-    users: {
-        schema: S.Schema({
-            id: S.Id(),
-            email: S.String(),
-            password: S.String(),
-            firstName: S.String(),
-            lastName: S.String(),
-            created_at: S.Date({ default: S.Default.now() }),
-        }),
-    },
-    conversations: {
-        schema: S.Schema({
-            id: S.Id(),
-            title: S.String(),
-            users: S.RelationMany('users', {
-                where: [['id', '=', '$id']],
-            }),
-            created_at: S.Date({ default: S.Default.now() }),
-        }),
-    },
-    messages: {
-        schema: S.Schema({
-            id: S.Id(),
-            content: S.String(),
-            user_id: S.String(),
-            user: S.RelationById('users', '$user_id'),
-            conversation_id: S.String(),
-            conversation: S.RelationById('conversations', '$conversation_id'),
-            created_at: S.Date({ default: S.Default.now() }),
-        }),
-    },
+  users: {
+    schema: S.Schema({
+      id: S.Id(),
+      email: S.String(),
+      password: S.String(),
+      firstName: S.String(),
+      lastName: S.String(),
+      created_at: S.Date({ default: S.Default.now() }),
+    }),
+  },
+  user_conversation: {
+    schema: S.Schema({
+      id: S.Id(),
+      user_id: S.String(),
+      user: S.RelationById('users', '$user_id'),
+      conversation_id: S.String(),
+      conversation: S.RelationById('conversations', '$conversation_id'),
+    }),
+  },
+  conversations: {
+    schema: S.Schema({
+      id: S.Id(),
+      title: S.String(),
+      created_at: S.Date({ default: S.Default.now() }),
+    }),
+  },
+  messages: {
+    schema: S.Schema({
+      id: S.Id(),
+      content: S.String(),
+      user_id: S.String(),
+      user: S.RelationById('users', '$user_id'),
+      conversation_id: S.String(),
+      conversation: S.RelationById('conversations', '$conversation_id'),
+      created_at: S.Date({ default: S.Default.now() }),
+    }),
+  },
 } satisfies ClientSchema;
