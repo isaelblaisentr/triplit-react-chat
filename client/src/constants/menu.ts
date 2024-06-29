792f0b4c791dcadc7c7209f2c78a2908a1cb7d4c
isabla <blais.isael.entr@gmail.com>
2024-06-29 17:44:35 -0400

    Add Conversation menu in SideBar

--- client/src/constants/menu.ts
+++ client/src/constants/menu.ts
@@ -1,11 +1,19 @@
 import { IMenuItem } from '../types';
 import { ROUTES } from './routes';
-import { UsersIcon } from '@heroicons/react/24/solid';
+import {
+  UsersIcon,
+  ChatBubbleBottomCenterIcon,
+} from '@heroicons/react/24/solid';
 
 export const MENU_LIST: IMenuItem[] = [
   {
     route: ROUTES.users,
     literal: 'Users',
     Icon: UsersIcon,
+  },
+  {
+    route: ROUTES.conversations,
+    literal: 'Conversations',
+    Icon: ChatBubbleBottomCenterIcon,
   },
 ];
