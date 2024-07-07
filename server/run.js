883313d48f2a276e4440917b44459965efc31bbe
isabla <blais.isael.entr@gmail.com>
2024-07-07 14:16:16 -0400

    Add run.js file with JWT secrets init.

---
+++ server/run.js
@@ -0,0 +1,44 @@
+import { createServer } from '@triplit/server';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config()
+
+console.log("PROJECT_ID: ", process.env.PROJECT_ID);
+console.log("JWT_SECRET: ", process.env.JWT_SECRET);
+
+const anonKey = jwt.sign(
+    {
+        'x-triplit-token-type': 'anon',
+        'x-triplit-project-id': process.env.PROJECT_ID,
+    },
+    process.env.JWT_SECRET
+);
+
+const serviceKey = jwt.sign(
+    {
+        'x-triplit-token-type': 'secret',
+        'x-triplit-project-id': process.env.PROJECT_ID,
+    },
+    process.env.JWT_SECRET
+);
+
+console.log("anonKey: ", anonKey)
+console.log("serviceKey: ", serviceKey)
+
+const port = +(process.env.PORT || 6543);
+
+const startServer = createServer({
+    //storage: 'sqlite',
+    verboseLogs: true,
+});
+
+const dbServer = startServer(port);
+
+console.log('running on port', port);
+process.on('SIGINT', function () {
+    dbServer.close(() => {
+        console.log('Shutting down server... ');
+        process.exit();
+    });
+});
